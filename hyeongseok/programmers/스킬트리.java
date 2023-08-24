0a9fcdcea26e1f18679b58ff41023928200d5e70
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2023-08-24 21:05:48 +0900

    feat : programmers 스킬트리 풀이 완료 close #15

--- hyeongseok/programmers/스킬트리.java
+++ hyeongseok/programmers/스킬트리.java
@@ -1,17 +1,41 @@
 package hyeongseok.programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 스킬트리 {
 
-	public int[] alpha;
-
-	// "CBD"	["BACDE", "CBADF", "AECB", "BDA"]
 	public int solution(String skill, String[] skill_trees) {
-		int answer = -1;
+		int answer = 0;
 
-		alpha = new int[skill.length()];
+		boolean[] alpha = new boolean[26];
 
 		for (int i = 0 ; i < skill.length() ; i++) {
-			alpha[skill.charAt(i) - 'A'] = i+1;
+			alpha[skill.charAt(i) - 'A'] = true;
+		}
+
+		for (int i = 0 ; i < skill_trees.length ; i++) {
+			String str = skill_trees[i];
+
+			String result = "";
+
+			for (int j = 0 ; j < str.length() ; j++) {
+				if (alpha[str.charAt(j) - 'A']) {
+					result += str.charAt(j);
+				}
+			}
+
+			boolean check = true;
+
+			for (int j = 0 ; j < result.length() ; j++) {
+				if (result.charAt(j) != skill.charAt(j)) {
+					check = false;
+					break;
+				}
+			}
+
+			if (check)
+				answer++;
 		}
 
 		return answer;
