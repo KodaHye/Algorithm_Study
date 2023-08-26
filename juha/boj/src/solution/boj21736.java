cba1e7532aeb540ab1e716be462d0864feb0153a
JUHA KIM <105307294+Holdm2t1ght@users.noreply.github.com>
2023-08-27 03:54:54 +0900

    Feat: BOJ 21736 헌내기는 친구가 필요해 풀이 완료 close #32
    이슈 번호 수정

--- juha/boj/src/solution/boj21736.java
+++ juha/boj/src/solution/boj21736.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class boj21736 {
     static int N, M;
