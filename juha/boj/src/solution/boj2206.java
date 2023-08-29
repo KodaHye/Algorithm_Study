ad693ca018d9e2ee99630edfa6a82323c0be25de
kimjuha <dnsfudcjsfud@gmail.com>
2023-08-29 17:59:30 +0900

    Feat: BOJ 2206 벽 부수고 이동하기 임시 저장

---
+++ juha/boj/src/solution/boj2206.java
@@ -0,0 +1,18 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj2206 {
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("boj/input/boj2206.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
