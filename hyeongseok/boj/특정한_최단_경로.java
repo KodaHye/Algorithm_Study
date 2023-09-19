d4c1336ab10c0c51c4acf851cfd6f66124fc4914
이형석 <gudtjr2949@naver.com>
2023-09-19 09:40:27 +0900

    feat: BOJ 1504 특정한 최단 경로 풀이 완료 #close 124

--- hyeongseok/boj/특정한_최단_경로.java
+++ hyeongseok/boj/특정한_최단_경로.java
@@ -6,82 +6,118 @@ import java.util.*;
 
 public class 특정한_최단_경로 {
 
-    static int N, E, requirement1, requirement2;
-    static int[] dist;
-    static List<List<Node>> list;
+	static int N, E;
+	static List<List<Node>> list;
 
-    static class Node implements Comparable<Node> {
-        int idx, cost;
+	static class Node implements Comparable<Node> {
+		int idx, cost;
 
-        public Node(int idx, int cost) {
-            this.idx = idx;
-            this.cost = cost;
-        }
+		public Node(int idx, int cost) {
+			this.idx = idx;
+			this.cost = cost;
+		}
 
-        @Override
-        public int compareTo(Node n) {
-            return this.cost - n.cost;
-        }
-    }
+		@Override
+		public int compareTo(Node n) {
+			return this.cost - n.cost;
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(bf.readLine());
+		StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 
-        list = new ArrayList<>();
+		list = new ArrayList<>();
 
-        dist = new int[N+1];
+		for (int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
+		}
 
-        for (int i = 0 ; i <= N ; i++) {
-            list.add(new ArrayList<>());
-        }
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(bf.readLine());
 
-        for (int i = 0 ; i < E ; i++) {
-            st = new StringTokenizer(bf.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+			list.get(a).add(new Node(b, c));
+			list.get(b).add(new Node(a, c));
+		}
 
-            list.get(a).add(new Node(b, c));
-            list.get(b).add(new Node(a, c));
-        }
+		st = new StringTokenizer(bf.readLine());
 
-        st = new StringTokenizer(bf.readLine());
+		int v1 = Integer.parseInt(st.nextToken());
+		int v2 = Integer.parseInt(st.nextToken());
 
-        requirement1 = Integer.parseInt(st.nextToken());
-        requirement2 = Integer.parseInt(st.nextToken());
+		boolean distancePossible_1 = true;
 
-        Dijk(requirement1);
-        Dijk(requirement2);
+		boolean distancePossible_2 = true;
 
-        System.out.println(Arrays.toString(dist));
-//        System.out.println(dist[N]);
-    }
+		int distance_1 = Dijk(1, v1);
+		int distance_2 = Dijk(v1, v2);
+		int distance_3 = Dijk(v2, N);
 
-    static void Dijk(int requirement) {
-        Queue<Node> PQ = new PriorityQueue<>();
+		if (distance_1 == Integer.MAX_VALUE || distance_2 == Integer.MAX_VALUE || distance_3 == Integer.MAX_VALUE) {
+			distancePossible_1 = false;
+		}
 
-        PQ.add(new Node(1, 0));
+		int distance_4 = Dijk(1, v2);
+		int distance_5 = Dijk(v2, v1);
+		int distance_6 = Dijk(v1, N);
 
-        Arrays.fill(dist, 10000);
-        boolean[] visited = new boolean[N+1];
+		if (distance_4 == Integer.MAX_VALUE || distance_5 == Integer.MAX_VALUE || distance_6 == Integer.MAX_VALUE) {
+			distancePossible_2 = false;
+		}
 
-        dist[1] = 0;
+		int answer = 0;
 
-        int visitedIdx = 0;
+		if (!distancePossible_1 && !distancePossible_2) {
+			answer = -1;
+		} else if (!distancePossible_1) {
+			answer = distance_4 + distance_5 + distance_6;
+		} else if (!distancePossible_2) {
+			answer = distance_1 + distance_2 + distance_3;
+		} else {
+			answer = Math.min(distance_1 + distance_2 + distance_3, distance_4 + distance_5 + distance_6);
+		}
 
-        while (!PQ.isEmpty()) {
-            Node n = PQ.poll();
+		System.out.println(answer);
+	}
 
-            int now = n.idx;
-            int distance = n.cost; // now 까지 오는데 지나온 거리
+	static int Dijk(int start, int end) {
+		Queue<Node> PQ = new PriorityQueue<>();
 
+		PQ.add(new Node(start, 0));
 
-        }
-    }
+        int[] dist = new int[N + 1];
+
+		Arrays.fill(dist, Integer.MAX_VALUE);
+
+		boolean[] visited = new boolean[N + 1];
+
+		dist[start] = 0;
+
+		while (!PQ.isEmpty()) {
+			Node n = PQ.poll();
+
+			int now = n.idx;
+			int distance = n.cost; // now 까지 오는데 지나온 거리
+
+            if (visited[now]) continue;
+            visited[now] = true;
+
+            for (Node nextNode : list.get(now)) {
+                if (!visited[nextNode.idx] && dist[nextNode.idx] > dist[now] + nextNode.cost) {
+					dist[nextNode.idx] = dist[now] + nextNode.cost;
+					PQ.add(new Node(nextNode.idx, dist[nextNode.idx]));
+				}
+            }
+		}
+
+		return dist[end];
+	}
 }
